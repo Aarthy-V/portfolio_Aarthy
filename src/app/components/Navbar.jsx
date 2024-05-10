0c0b19b355861f76a06872460974541e11184ff3
Aarthy <2019e001@eng.jfn.ac.lk>
2024-05-10 22:38:42 +0530

    navlink

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import Link from 'next/link'
+import NavLink from './NavLink'
 
+const navLinks = [
+    {
+        title: "About",
+        path: "#about",
+    },
+    {
+        title: "Projects",
+        path: "#projects",
+    },
+    {
+        title: "Contact",
+        path: "#contact",
+    }
+
+]
 const Navbar = () => {
   return (
     <nav>
@@ -8,11 +24,13 @@ const Navbar = () => {
             <Link href={'/'} className='text-5xl text-white font-semibold'>LOGO</Link>
             <div className='menu'>
                 <ul>
-                    <li>
-                        <Link href={"#about"} className='block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white'>
-                            About
-                        </Link>
-                    </li>
+                    {
+                        navLinks.map((link, index)=>(
+                            <li key={index}>
+                                <NavLink href={link.path} title={link.title} />
+                            </li>
+                        ))
+                    }
                 </ul>
             </div>
         </div>
