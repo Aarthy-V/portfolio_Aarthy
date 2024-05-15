e3e9af4fe0f71537bb16288957b4dca85bb70564
Aarthy <2019e001@eng.jfn.ac.lk>
2024-05-15 22:13:17 +0530

    Email section

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -6,8 +6,11 @@ import Image from 'next/image';
 
 const EmailSection = () => {
     return (
-        <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
-            <div>
+        <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative'>
+            <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 translate-1/2'>
+
+            </div>
+            <div className='z-10'>
                 <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
                 <p className='text-[#ADB7BE] mb-4 max-w-md'>
                     {" "}
@@ -16,18 +19,18 @@ const EmailSection = () => {
                     hi, I'll try my best to get back to you!
                 </p>
                 <div className='socials flex flex-row gap-2'>
-                    <Link href="github.com">
-                        <Image src={github} alt="Git" />
+                    <Link href="github.com" className='h-10 w-10'>
+                        <Image src={github} alt="Git" style={{ filter: 'invert(100%)' }}/>
                     </Link>
-                    <Link href="linkedin.com">
-                        <Image src={linkedin} alt="Linked In" />
+                    <Link href="linkedin.com" className='h-10 w-10'>
+                        <Image src={linkedin} alt="Linked In" style={{ filter: 'invert(100%)' }} />
                     </Link>
                 </div>
             </div>
             <div>
                 <form className='flex flex-col'>
-                    <div>
-                        <label htmlfor="email" className='text-white block text-sm font-medium'>Your Email</label>
+                    <div className='mb-6'>
+                        <label htmlFor="email" className='text-white block mb-2 text-sm font-medium'>Your Email</label>
                         <input
                             type="email"
                             id="email"
@@ -37,7 +40,7 @@ const EmailSection = () => {
                         />
                     </div>
                     <div className='mb-6'>
-                        <label htmlfor="subject" className='text-white block text-sm font-medium'>Subject</label>
+                        <label htmlFor="subject" className='text-white block mb-2 text-sm font-medium'>Subject</label>
                         <input
                             type="text"
                             id="subject"
@@ -46,6 +49,26 @@ const EmailSection = () => {
                             placeholder='Just say Hi...'
                         />
                     </div>
+                    <div className='mb-6'>
+                        <label
+                            htmlFor="message"
+                            className='text-white block text-sm mb-2 font-medium'
+                        >
+                            Message
+                        </label>
+                        <textarea
+                            name="message"
+                            id="message"
+                            className='bg-[#18191E] border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+                            placeholder="Let's talk about ..."
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'
+                    >
+                        Send Message
+                    </button>
                 </form>
             </div>
         </section>
