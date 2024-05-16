c06e9f9820556c9c08d4fed74e0880b6c2b06d3f
Aarthy <2019e001@eng.jfn.ac.lk>
2024-05-16 13:12:19 +0530

    minor chnges and animations

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,7 +1,8 @@
 "use client"
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import ProjectCard from './ProjectCard'
 import ProjectTag from './ProjectTag'
+import {motion, useInView} from "framer-motion"
 
 const projectsData = [
     {
@@ -32,6 +33,8 @@ const projectsData = [
 
 const ProjectsSection = () => {
     const [tag, setTag] = useState("All");
+    const ref=useRef(null);
+    const isInView=useInView(ref, {once:true});
 
     const handleTagChange = (newTag) => {
         setTag(newTag);
@@ -41,8 +44,13 @@ const ProjectsSection = () => {
         project.tag.includes(tag)
     );
 
+    const cardVariants ={
+        initial: {y:50, opacity:0},
+        animate: {y:0, opacity:1},
+    };
+
     return (
-        <>
+        <section>
             <h2 className='text-center text-4xl font-bold text-white mt-4'> 
                 My Projects
             </h2>
@@ -63,8 +71,15 @@ const ProjectsSection = () => {
                     isSelected={tag==="ML"}
                 />
             </div>
-            <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
-                {filteredProjects.map((project)=> (
+            <ul ref={ref} className='grid md:grid-cols-3 gap-8 md:gap-12'>
+                {filteredProjects.map((project, index)=> (
+                    <motion.li 
+                        key={index} 
+                        variants={cardVariants} 
+                        initial="initial" 
+                        animate={isInView?"animate":"initial"}
+                        transition={{duration:0.3, delay:index*0.4}}
+                    >
                     <ProjectCard 
                         key={project.id} 
                         title={project.title} 
@@ -72,9 +87,10 @@ const ProjectsSection = () => {
                         imgurl={project.image}
                         giturl={project.giturl}
                     />
+                    </motion.li>
                 ))}
-            </div>
-        </>
+            </ul>
+        </section>
     )
 }
 
